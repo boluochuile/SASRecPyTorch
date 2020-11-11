d25c3bab687381e2b02412946d0ffecd55b9ba04
boluochuile <ll_best0110@163.com>
2020-11-11 18:16:00 +0800

    modify

--- main.py
+++ main.py
@@ -100,14 +100,14 @@ if __name__ == '__main__':
                 loss += args.l2_emb * torch.norm(param)
             loss.backward()
             adam_optimizer.step()
-            print('loss: ', loss)
             # print('t_test')
             # model.eval()
             # t_test = evaluate(model, dataset, args)
             # model.train()
             # print("loss in epoch {} iteration {}: {}".format(epoch, step, loss.item())) # expected 0.4~0.6 after init few epochs
+        print('loss: ', loss)
 
-        if epoch % 20 == 0:
+        if epoch % 2 == 0:
             print('epoch')
             model.eval()
             t1 = time.time() - t0
